c431aa0e59c172fc2043e02abb2cc0c3d4e7221b
unknown <test@email.com>
2023-05-22 17:50:17 +0900

    fix bug file pom.xml

--- src/main/java/com/vtes/VtesApplication.java
+++ src/main/java/com/vtes/VtesApplication.java
@@ -1,13 +1,9 @@
-package com.vti;
+package com.vtes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = { "com.vti.*" ,"com.vti"})
-@EnableFeignClients
 public class VtesApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/vtes/entity/RefreshToken.java
+++ src/main/java/com/vtes/entity/RefreshToken.java
@@ -1,37 +1,41 @@
-package com.vti.entity;
+package com.vtes.entity;
 
 import java.sql.Date;
+import java.time.Instant;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity()
 @Table(name = "tbl_refresh_token")
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 public class RefreshToken {
 	@Id
-	@Column(name = "ID")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
-	
-	@Column(name = "ACCOUNT_ID")
-	@ManyToOne
-	private Account account;
-	@Column(name="TOKEN")
+	@Column(name = "ID")
+	private long id;
+
+	@OneToOne
+	@JoinColumn(name = "USER_ID", referencedColumnName = "ID")
+	private User user;
+
+	@Column(name = "TOKEN", nullable = false, unique = true)
 	private String token;
-	
-	@Column(name ="CREATE_TIME")
-	private Date createTimeDate;
-	
-	@Column(name ="UPDATE_TIME")
-	private Date updateTimeDate;
-	
+
+	@Column(name = "EXPIRY_DATE", nullable = false)
+	private Instant expiryDate;
+
 }
