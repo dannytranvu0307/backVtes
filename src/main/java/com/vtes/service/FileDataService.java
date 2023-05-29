e796d6b204a72b13a85b0ae288634c392bded465
chienVti <chien.tranvan@vti.com.vn>
2023-05-29 10:23:38 +0900

    fix FileDataService can not dowload file

--- src/main/java/com/vtes/service/FileDataService.java
+++ src/main/java/com/vtes/service/FileDataService.java
@@ -12,7 +12,6 @@ import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
